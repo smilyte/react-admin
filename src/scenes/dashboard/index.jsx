e7f237d8733d672df9c4b39570518892c8c82ba2
smilyte <smilyte@gmail.com>
2022-11-12 17:40:10 +0100

    sidebar, team and start of dashboard - video 1:45:54

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,15 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 
+import Header from "../../components/Header";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return (
+    <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center"></Box>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
+    </Box>
+    );
 }
 
 export default Dashboard;

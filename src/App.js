9f24d101d168ade1bb8a6b97a5e3c777de38c4fb
smilyte <smilyte@gmail.com>
2022-11-12 16:10:31 +0100

    topbar done 50:55 in tutorial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { ColorModeContext, useMode } from './theme';
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import Topbar from "./scenes/global/Topbar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
   const [theme, colorMode] = useMode();
-  
+
   return (
-  <ColorModeContext.ThemeProvider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <div className="app">
           <main className="content">
-
+            <Topbar />
           </main>
         </div>
-    </ThemeProvider>
-    </ColorModeContext.ThemeProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 

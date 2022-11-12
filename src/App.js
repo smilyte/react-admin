871f16b904c68b974fdd77e98a60536a2469ddbd
smilyte <smilyte@gmail.com>
2022-11-12 23:17:18 +0100

    calendar, vid 2:42

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
 
 
 function App() {
@@ -40,7 +40,7 @@ function App() {
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
